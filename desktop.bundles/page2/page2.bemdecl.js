13293d7d0c174b9bb9c4d173ca596c2476e39ecf
Varvara Stepanova <toivonen@toivonen.ru>
2014-01-21 09:34:08 +0100

    Compilation results

---
+++ desktop.bundles/page2/page2.bemdecl.js
@@ -0,0 +1,39 @@
+exports.blocks = [
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            },
+            {
+                "name": "meta"
+            },
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "header"
+    },
+    {
+        "name": "menu",
+        "elems": [
+            {
+                "name": "item"
+            }
+        ],
+        "mods": [
+            {
+                "name": "selected",
+                "vals": [
+                    false,
+                    true
+                ]
+            }
+        ]
+    },
+    {
+        "name": "footer"
+    }
+];
